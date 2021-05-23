12dd3a4c6975d0778643841f992a504b498ab963
biinggala <49203064+biinggala@users.noreply.github.com>
2021-05-23 16:51:44 +0900

    create fill/paint function, save function

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
+ctx.fillStyle = INITIAL_COLOR;
 
 let painting = false;
 let filling = false;
@@ -36,6 +42,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -43,7 +50,7 @@ function handleRangeChange(event) {
   ctx.lineWidth = width;
 }
 
-function handleMode(event) {
+function handleMode() {
   if (filling === true) {
     filling = false;
     mode.innerText = "fill";
@@ -53,11 +60,29 @@ function handleMode(event) {
   }
 }
 
+function handleCanvasClick() {
+  if (filling) ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+
+function handleCM(event) {
+  event.preventDefault();
+}
+
+function saveImg(event) {
+  const img = canvas.toDataURL("image/png");
+  const link = document.createElement("a");
+  link.href = img;
+  link.download = "창작교실";
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("mousedown", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach((color) =>
@@ -70,4 +95,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleMode);
+}
+
+if (save) {
+  save.addEventListener("click", saveImg);
 }
